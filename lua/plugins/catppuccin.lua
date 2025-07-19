11eadb7a2202458f4c944345695b6e873c30bcf3
nik <nik.kiene.76@gmail.com>
2025-07-19 13:16:33 +0200

    Toggle colortheme

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,6 +1,21 @@
-return { "catppuccin/nvim", name = "catppuccin", priority = 1000,
-	config = function() 
+return {
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	config = function()
 		require("catppuccin").setup()
-		vim.cmd.colorscheme("catppuccin-frappe") -- catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
-	end
+		local latte = "catppuccin-latte"
+		local mocha = "catppuccin-mocha"
+		local current_color_scheme
+		local toggleColorSchme = function()
+			if current_color_scheme == latte then
+				current_color_scheme = mocha
+			else
+				current_color_scheme = latte
+			end
+			vim.cmd.colorscheme(current_color_scheme) -- catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
+		end
+		toggleColorSchme()
+		vim.keymap.set("n", "<leader>tc", toggleColorSchme, { desc = "Toggle Theme" })
+	end,
 }
